ecd33424c1e479d74a498914b7817ce833ea8b60
bszeidl <szeidl.balazs@hotmail.com>
2022-12-15 14:58:22 +0100

    The horizontal and vertical lines are ready.

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 function pageLoaded () {
     var canvas = document.getElementById('myCanvas');
     var context = canvas.getContext('2d');
-    context.fillRect(175,100,100,100);  
+   
+    let canvasWidth = canvas.width;
+    let canvasHeight = canvas.height;    
+
+    /*horizontal line*/
+    context.beginPath();
+    context.moveTo(0, canvasHeight *.5);
+    context.lineTo(canvasWidth, canvasHeight * .5);
+    context.strokeStyle = 'red';
+    context.stroke();
+
+    /*vertical line*/
+    context.beginPath();
+    context.moveTo(canvasWidth *.5, 0);
+    context.lineTo(canvasWidth * .5, canvasHeight);
+    context.strokeStyle = 'green';
+    context.stroke();
+
+    /*Before each line element.beginStyle() must be restarted otherwise the different lines will not be individuals and cannpt be formated independently. */
 
 }
 
